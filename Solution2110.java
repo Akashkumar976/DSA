96e13ab7708bbae4b4003d525d6ba5ac1c16e06d
Akashkumar976 <akashkumarsingh75062@gmail.com>
2025-12-15 20:14:12 +0530

    LeetCode2110

---
+++ Solution2110.java
@@ -0,0 +1,19 @@
+class    {
+    public long getDescentPeriods(int[] prices) {
+        long max=1;
+        long len=1;
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]==prices[i-1]-1)
+            {
+                len++;
+            }
+            else
+            {
+                len=1;
+            }
+            max+=len;
+        }
+        return max;
+    }
+}
